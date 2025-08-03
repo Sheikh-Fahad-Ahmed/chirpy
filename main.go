ffe571a07db3b26578d14415695f871187d5cf86
Fahad Ahmed <fahad838@gmail.com>
2025-08-03 10:35:54 +0530

    Create handlers for Hits, Create middleware, add healthz, reset endpoint

--- main.go
+++ main.go
@@ -1,21 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func myHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, req *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "ok")
+}
+
 func main() {
-	
+
+	apicfg := apiConfig{}
+
 	const port = "8080"
 	mux := http.NewServeMux()
-	
+
 	server := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", myHandler)
+	mux.HandleFunc("/metrics", apicfg.hitsHandler)
+	mux.HandleFunc("/reset", apicfg.resetHandler)
+
+	mux.Handle("/app/", apicfg.middlewareMetricInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir("."))),
+	))
 
 	log.Println("Started a server at port:", port)
 	log.Fatal(server.ListenAndServe())
