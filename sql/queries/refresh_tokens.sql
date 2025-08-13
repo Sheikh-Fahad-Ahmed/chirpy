711318446bc5716d864f9caf579e188b3e732969
Fahad Ahmed <fahad838@gmail.com>
2025-08-13 10:47:11 +0530

    Create query RecokeToken and generate Go code

--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -13,3 +13,9 @@ RETURNING *;
 SELECT * 
 FROM refresh_tokens
 WHERE token = $1;
+
+-- name: RevokeToken :exec
+UPDATE refresh_tokens
+SET revoked_at = NOW(),
+    updated_at = NOW()
+WHERE token = $1;
